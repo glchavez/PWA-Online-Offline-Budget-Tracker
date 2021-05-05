c3d3476e6a900a104ded5f32e2469b26a66ec70d
Giovany <giochavez320@gmail.com>
2021-05-05 17:35:59 -0600

    Starting service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,29 @@
+// install event handler
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('static').then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './index.js',
+                './styles.css',
+                './icons/icon-192x192.png',
+                './icons/icon-512x512.png',
+                '../models/transaction.js',
+                '../routes/api.js',
+                '../server.js'
+            ]);
+        })
+    );
+    console.log('Install');
+    self.skipWaiting();
+});
+
+// retrieve assets from cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
