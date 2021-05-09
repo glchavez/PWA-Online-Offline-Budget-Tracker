3fb46bc0a0eb7459ea3828d68f4510f902164705
Giovany <giochavez320@gmail.com>
2021-05-09 13:18:00 -0600

    Fixed service worker file and added manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,3 @@
-
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
@@ -7,15 +6,16 @@ const FILES_TO_CACHE = [
     './index.js',
     './styles.css',
     './icons/icon-192x192.png',
-    './icons/icon-512x512.png'
+    './icons/icon-512x512.png',
+    './indexedDb.js'
 ];
 
 // install
 self.addEventListener("install", function (evt) {
     // pre cache image data
-    evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-    );
+    // evt.waitUntil(
+    //     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/"))
+    // );
 
     // pre cache all static assets
     evt.waitUntil(
